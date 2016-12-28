b89a2837774ec24613b4c3ade1c1fe7b4a9f7c41
alexey <saraevalex@gmail.com>
2016-12-28 18:17:27 +0200

    zz

---
+++ hm4/hm4/Message.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Message.H"
+
+
+Message::Message(string source, string subject, string content) : source_(source), subject_(subject), content_(content), read_(false) {}
+
+void Message::Display(int num) const
+{
+	cout << num << ") "<< (read_ ? "" : "(Unread) ") << "From: " << source_ << endl;
+	cout << "Subject: " << subject_ << endl;
+}
+
+void Message::Read()
+{
+    read_ = true;
+	cout << "From: " << source_ << endl;
+	cout << "Subject: " << subject_ << endl;
+	cout << "Content: " << content_ << endl;
+}
+
+bool Message::isRead()
+{
+	return read_;
+}
