c88f57e63a936af8ef48a365d70b3b41ec519692
alexey <saraevalex@gmail.com>
2017-01-04 23:09:14 +0200

    zz

--- hm4/hm4/Lists.cpp
+++ hm4/hm4/Lists.cpp
@@ -5,15 +5,15 @@
 using std::cout;
 
 void list::prepend(Message& f) {                    //$$$$$$$$$$$$$$$ int only for testing - change to Follower later
-	listelem* temp = new listelem; // allocation
-	temp->next = h;
+	Node* temp = new Node; // allocation
+	temp->next = head;
 	temp->data = &f;  //  NEW Syntax 
-	h = temp;
+	head = temp;
 	list::size = list::size++;
 }
 /* this function works for int - need special modifications for other types*/
 void list::print() const {
-	listelem* temp = h;
+	Node* temp = head;
 	while (temp != 0) {
 		cout << temp->data << "\n";
 		temp = temp->next;
@@ -21,19 +21,19 @@ void list::print() const {
 }
  
 void list::delete_head() {
-	listelem* temp = h;
-	h = h->next;
+	Node* temp = head;
+	head = head->next;
 	//delete temp->data;
 	delete temp;                   //free memory
 }
 
 void list::release() {
-	while (h != 0)
+	while (head != 0)
 		delete_head();
 }
 
 void list::go_to_first() {
-	list::iterator = list::h;
+	list::iterator = list::head;
 }
 
 void list::next() {
@@ -50,11 +50,11 @@ int list::get_size() {
 
 void list::delete_current() {
 
-	if (list::iterator == list::h)
+	if (list::iterator == list::head)
 		list::delete_head();
 	else
 	{
-		listelem* temp = list::h;
+		Node* temp = list::head;
 		while (temp->next != list::iterator)                //searching for the element before the deleted one
 		{
 			temp = temp->next;
@@ -67,6 +67,73 @@ void list::delete_current() {
 	}
 }
 
+// here start implementation of follower list &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+/*
+void list_f::prepend(Follower& f) {                    
+	Node_f* temp = new Node_f; // allocation
+	temp->next = head;
+	temp->data = &f;  //  NEW Syntax 
+	head = temp;
+	list_f::size = list_f::size++;
+}
+/* this function works for int - need special modifications for other types
+void list_f::print() const {
+	Node_f* temp = head;
+	while (temp != 0) {
+		cout << temp->data << "\n";
+		temp = temp->next;
+	}
+}
+
+void list_f::delete_head() {
+	Node_f* temp = head;
+	head = head->next;
+	//delete temp->data;
+	delete temp;                   //free memory
+}
+
+void list_f::release() {
+	while (head != 0)
+		delete_head();
+}
+
+void list_f::go_to_first() {
+	list_f::iterator = list_f::head;
+}
+
+void list_f::next() {
+	list_f::iterator = list_f::iterator->next;
+}
+
+Follower* list_f::get_current() {                          //$$$$$$$$$$$$$$$ int only for testing - change to Follower later
+	return list_f::iterator->data;
+}
+
+int list_f::get_size() {
+	return list_f::size;
+}
+
+void list_f::delete_current() {
+
+	if (list_f::iterator == list_f::head)
+		list_f::delete_head();
+	else
+	{
+		Node_f* temp = list_f::head;
+		while (temp->next != list_f::iterator)                //searching for the element before the deleted one
+		{
+			temp = temp->next;
+		}
+		//here we found the element that is previous to the one that we need to delete
+		temp->next = list_f::iterator->next;
+		delete (list_f::iterator);
+		list_f::size--;
+		list_f::iterator = NULL;
+	}
+}
+*/
+
+/*
 int main() {
 	list *p; {
 		list w;
@@ -84,9 +151,9 @@ int main() {
 		w.go_to_first();
 		w.next();
 
-		Message *tmp4 = w.get_current();
+		Message *curent = w.get_current();
 		w.delete_current();
-		/*
+		
 		w.prepend(6);
 		w.prepend(10);
 		w.prepend(15);
@@ -99,8 +166,9 @@ int main() {
 		cout << "deleted\n";
 		w.release();
 		//w.print();
-		*/
+		
 	}
 
 	return 0;
 }
+*/
--- hm4/hm4/Message.cpp
+++ hm4/hm4/Message.cpp
@@ -22,3 +22,28 @@ bool Message::isRead()
 {
 	return read_;
 }
+
+//////////////////    Message Box
+/*
+void MessageBox::Add(Message newMessage)
+{
+	MessageBox::Msges.prepend(newMessage);
+}
+
+
+
+int main() {
+
+	Message *tmp1, *tmp2, *tmp3;
+	tmp1 = new Message("alibaba", "shodedim", "tellmestory"); // aloocita
+	tmp2 = new Message("Shakira", "Eminem", "Soy Mujera"); // aloocita
+	tmp3 = new Message("Betty", "Bam", "ohevet bambam"); // aloocita
+
+	MessageBox box;
+
+	box.Add(tmp1);
+
+
+	return 0;
+}
+*/
